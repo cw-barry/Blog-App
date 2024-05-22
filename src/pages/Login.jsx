6ddf93227f130de7f75f4f2e17801c53852fd56b
cw-barry <barry@clarusway.com>
2024-05-22 18:21:02 -0400

    update login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,13 @@
-import React, { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Button, Form, FormGroup, Input, Label } from "reactstrap";
-import { AuthContext } from "../context/AuthContext";
+import React, { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
+import { AuthContext } from '../context/AuthContext';
 
 const Login = () => {
   const { loginUser } = useContext(AuthContext);
   const [formData, setFormData] = useState({
-    email: "barry4@gmail.com",
-    password: "qazqwe123",
+    email: 'barry4@gmail.com',
+    password: '',
   });
 
   const navigate = useNavigate();
@@ -27,16 +27,16 @@ const Login = () => {
         src="https://images.pexels.com/photos/14186492/pexels-photo-14186492.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         alt="login_page_image"
         className="col-8 d-none d-md-block"
-        style={{ height: "100vh" }}
+        style={{ height: '100vh' }}
       />
 
       <Form
         onSubmit={handleSubmit}
         className="col-12 col-md-4 d-flex justify-content-center flex-column align-items-center"
-        style={{ height: "100vh" }}
+        style={{ height: '100vh' }}
       >
         <p className="fs-2">Login Page</p>
-        <FormGroup style={{ width: "18rem" }}>
+        <FormGroup style={{ width: '18rem' }}>
           <Label for="email">Enter Your Email</Label>
           <Input
             type="email"
@@ -46,7 +46,7 @@ const Login = () => {
             onChange={handleChange}
           />
         </FormGroup>
-        <FormGroup style={{ width: "18rem" }}>
+        <FormGroup style={{ width: '18rem' }}>
           <Label for="password">Enter Your Password</Label>
           <Input
             type="password"
@@ -56,11 +56,11 @@ const Login = () => {
             onChange={handleChange}
           />
         </FormGroup>
-        <Button color="secondary" style={{ width: "18rem" }}>
+        <Button color="secondary" style={{ width: '18rem' }}>
           Log in
         </Button>
         <p className="my-3">Or</p>
-        <Button color="primary" style={{ width: "18rem" }}>
+        <Button color="primary" style={{ width: '18rem' }}>
           Log in With Google
         </Button>
         <p className="mt-4">
